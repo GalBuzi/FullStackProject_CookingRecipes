abbdf3004ca9540953b52ef43df181ada504f53c
GalBuzi <galbuz@post.bgu.ac.il>
2020-06-22 20:24:38 +0300

    cookie issue

--- src/main.js
+++ src/main.js
@@ -73,8 +73,8 @@ const router = new VueRouter({
 });
 
 const shared_data = {
-  server: "https://assignment3-2-gal.herokuapp.com",
-  // server: "http://localhost:8008",
+  // server: "https://assignment3-2-gal.herokuapp.com",
+  server: "http://localhost:8008",
   username: localStorage.username,
   login(username) {
     localStorage.setItem("username", username);
--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
     // host: "localhost",
     proxy: {
       "/": {
-        target: "https://assignment3-2-gal.herokuapp.com",
-        // target: "http://localhost:8008",
+        // target: "https://assignment3-2-gal.herokuapp.com",
+        target: "http://localhost:8008",
         ws: true,
         changeOrigin: true,
       },
